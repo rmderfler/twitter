2a73a97691b64d2ab062b006c570ad77a16e4169
Derfler <rmderfler@gmail.com>
2014-09-07 17:56:09 -0700

    install devise user authentication

---
+++ Gemfile
@@ -0,0 +1,40 @@
+source 'https://rubygems.org'
+
+gem 'rails'
+gem 'pg'
+gem 'bootstrap-sass'
+gem 'sprockets'
+gem 'sass-rails'
+gem 'uglifier'
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'simple_form'
+gem 'bcrypt'
+gem 'textacular', '~> 3.0'
+gem 'devise'
+
+group :production do
+  gem 'rails_12factor'
+end
+
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'quiet_assets'
+  gem "letter_opener"
+end
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem "factory_girl_rails", "~> 4.0"
+end
+
+group :test do
+  gem 'shoulda-matchers'
+  gem 'capybara'
+  gem 'poltergeist'
+  gem 'launchy'
+  gem 'database_cleaner'
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,60 @@
+class UsersController < ApplicationController
+  before_filter :authorize, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "User was added"
+      redirect_to session_user_path(session, @user)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @current_user.id == @user.id
+      if @user.update(user_params)
+        @user.update_attribute(:email, params[:user])
+        flash[:notice] = "User updated."
+        redirect_to session_user_path(session, @user)
+      else
+        render 'edit'
+      end
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @current_user.id == @user.id
+      if @user.destroy
+        current_user.destroy
+        session[:user_id] = nil
+        flash[:notice] = "The user was deleted."
+        redirect_to root_path
+      end
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+end
